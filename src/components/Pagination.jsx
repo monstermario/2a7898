6d0b612fa441ca1b4cd777505a052443b678f4c1
trustsuperdev <mw.topdev@gmail.com>
2022-06-19 19:22:31 -0700

    add function for pagenation

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -42,7 +42,7 @@ function Pagination({
           // Do not remove the aria-label below, it is used for Hatchways automation.
           aria-label="Goto previous page"
           onClick={onPrevious}
-          disabled={false} // change this line to disable a button.
+          disabled={currentPage === 1} // change this line to disable a button.
         >
           <ChevronLeftIcon />
         </button>
@@ -50,7 +50,6 @@ function Pagination({
 
       {paginationRange.map((pageNumber) => {
         const key = nanoid();
-
         if (pageNumber === DOTS) {
           return (
             <li key={key} className="dots">
@@ -63,7 +62,7 @@ function Pagination({
           <li
             key={key}
             className="paginationItem"
-            aria-current="false" // change this line to highlight a current page.
+            aria-current={currentPage === pageNumber? 'page': false} // change this line to highlight a current page.
           >
             <button
               type="button"
@@ -84,7 +83,7 @@ function Pagination({
           // Do not remove the aria-label below, it is used for Hatchways automation.
           aria-label="Goto next page"
           onClick={onNext}
-          disabled={false} // change this line to disable a button.
+          disabled={currentPage === Math.ceil(totalCount / pageSize)} // change this line to disable a button.
         >
           <ChevronRightIcon />
         </button>
